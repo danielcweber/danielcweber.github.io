8d7c53a565d57ad05c33324d910727a8b3a5a555
Daniel Weber <dw@exram.de>
2021-01-07 20:02:58 +0100

    Add a VS-solution for sample code.

---
+++ code/BlogCode/BlogCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlogCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
